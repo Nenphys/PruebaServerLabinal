f69d937b62d04e4fa992dfbec1f553acf5e78f9f
Jorge <jfcc.chavez@gmail.com>
2013-09-27 18:47:04 -0600

    Servidor http trabajando correctamente

--- Server.py
+++ Server.py
@@ -1,5 +1,18 @@
 __author__ = 'Chapo'
 
-print ("hola")
+from socket import *
 
-print("Adios")
+
+s = socket (AF_INET, SOCK_STREAM)
+s.bind(('',8080))
+s.listen(1)
+
+while True:
+    client,Ip = s.accept()
+    print("Conexion hecha por %s " % str(Ip))
+    client.send(("HTTP/1.1 200 OK"+"\x0D\x0A"+
+                 "\x0D\x0A"+
+                 "Respuesta=9").encode())
+
+    client.close()
+
