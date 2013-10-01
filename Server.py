d2457fae5a8b585e5692bf98abe96a14ef423456
Jorge <jfcc.chavez@gmail.com>
2013-09-30 22:59:26 -0600

    Servidor http trabajando correctamente

--- Server.py
+++ Server.py
@@ -6,6 +6,7 @@ from socket import *
 s = socket (AF_INET, SOCK_STREAM)
 s.bind(('',8080))
 s.listen(1)
+comd = 0
 
 while True:
     client,Ip = s.accept()
@@ -13,6 +14,17 @@ while True:
     client.send(("HTTP/1.1 200 OK"+"\x0D\x0A"+
                  "\x0D\x0A"+
                  "Respuesta=9").encode())
+    #if comd == 0:
+    #    client.send(("HTTP/1.1 200 OK"+"\x0D\x0A"+
+    #             "\x0D\x0A"+
+    #             "Respuesta=50x01,0x06,0x00,0x02,0x00,0x80").encode())
+    #    comd = 1
+    #else:
+    #    client.send(("HTTP/1.1 200 OK"+"\x0D\x0A"+
+    #             "\x0D\x0A"+
+    #             "Respuesta=50x01,0x06,0x00,0x02,0x00,0x00").encode())
+    #    comd = 0
+
 
     client.close()
 
